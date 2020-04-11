75ef7ed9b69048d664f6a1af5f142a68dfc89094
Olga <olga.faddeenkov@tum.de>
2020-04-11 11:27:15 +0200

    added more tests

--- test/jsonParser_test.ml
+++ test/jsonParser_test.ml
@@ -15,9 +15,26 @@ let query1 = {sel = [Type_sel; ID_sel; Name_sel];
               str = None_s;
               lim = Constraint_c("tmp == 'E'")}
 
+let query2 = {sel = [Name_sel; Location_sel; Type_sel; ID_sel];
+              k = Var_k;
+              tar = And_t(["a"; "b"]);
+              f = Uses_f;
+              str = Fun_s("ggt");
+              lim = Constraint_c("a/b == 2")}
+
+let query3 = {sel = [Location_sel];
+              k = Fun_k;
+              tar = All_t;
+              f = Returns_f;
+              str = None_s;
+              lim = None_c}
+
 let tests = "test suite for JSONParser" >::: [
   "typical query"  >:: (fun _ -> assert_equal query (parse_json_file "test/test.json"));
-  "random lowcase and uppercase" >:: (fun _ -> assert_equal query1 (parse_json_file "test/test1.json"))
+  "random lowcase and uppercase" >:: (fun _ -> assert_equal query1 (parse_json_file "test/test1.json"));
+  "query with and" >:: (fun _ -> assert_equal query2 (parse_json_file "test/test2.json"));
+  "function-returns query" >:: (fun _ -> assert_equal query3 (parse_json_file "test/test3.json"));
+  "bad query" >:: (fun _ -> assert_raises (SyntaxError("Wrong syntax: unexpected input for structure")) (fun _ -> (parse_json_file "test/test4.json")))
 ]
 
 let _ = run_test_tt_main tests
